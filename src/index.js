98322bd3c73feb9e5620d6bb2af52d6274c6efc3
Kuzya <alexkuz@gmail.com>
2016-03-10 19:16:11 +0300

    fix dependencies

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ export Autosize from './Autosize';
 export Autocomplete from './Autocomplete';
 export Dropdown from './Dropdown';
 export Combobox from './Combobox';
+export Mask from './Mask';
+export DatePicker from './DatePicker';
